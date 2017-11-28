a48955a46e6c72f447c32e8676dab78d986a5d4d
dtosca <dtosca@wellesley.edu>
2017-11-28 15:40:33 -0500

    Updated nothing

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # Diana Tosca and Jacqueline Young
 import sys
-import MySQLdb
 import dbconn2
-import hwk6
+import houseIt
+import MySQLdb
 import os
 from flask import (Flask, render_template, make_response,
                     url_for, request, flash, redirect)
@@ -15,6 +15,25 @@ def index():
     ''' Index page
     ''' 
     return render_template('index.html')
+
+@app.route('/dorms/', methods= ['GET','POST'])
+def dorms():
+    ''' Dorms page
+    ''' 
+    return render_template('dorms.html')
+
+@app.route('/faq/', methods= ['GET','POST'])
+def faq():
+    ''' FAQ page
+    ''' 
+    return render_template('faq.html')
+
+@app.route('/preferences/', methods= ['GET','POST'])
+def pref():
+    ''' Preferences page
+    ''' 
+    return render_template('preferences.html')
+
 
 if __name__ == '__main__':
     app.debug = True
