ded1efe1b3d5ce5740a3b9bdc5b3e0a7e6e47643
team Diana Tosca <houseit@tempest.wellesley.edu>
2017-12-12 15:22:41 -0500

    Adding login

--- app.py
+++ app.py
@@ -4,13 +4,54 @@ import dbconn2
 import dbfunctions
 import MySQLdb
 import os
-from flask import (Flask, render_template, make_response,
-                    url_for, request, flash, redirect)
+import imghdr
+from flask import (Flask, render_template, make_response, request, redirect, url_for,session, flash, send_from_directory)
+from flask_cas import CAS
 
 app = Flask(__name__)
 app.secret_key = 'b@n@N@'
 
-@app.route('/', methods= ['GET','POST'])
+CAS(app)
+app.config['CAS_SERVER'] = 'https://login.wellesley.edu:443'
+app.config['CAS_AFTER_LOGIN'] = 'logged_in'
+app.config['CAS_LOGIN_ROUTE'] = '/module.php/casserver/cas.php/login'
+#app.config['CAS_LOGOUT_ROUTE'] = '/module.php/casserver/cas.php/logout'
+app.config['CAS_AFTER_LOGOUT'] = 'https://cs.wellesley.edu:1942/scott'
+app.config['CAS_VALIDATE_ROUTE'] = '/module.php/casserver/serviceValidate.php'
+
+@app.route('/logged_in/')
+def logged_in():
+    flash('successfully logged in!')
+    return redirect( url_for('index.html') )
+ 
+@app.route('/')
+def index():
+    print session.keys()
+    for k in session.keys():
+        print k,' => ',session[k]
+    if '_CAS_TOKEN' in session:
+        token = session['_CAS_TOKEN']
+    if 'CAS_ATTRIBUTES' in session:
+        attribs = session['CAS_ATTRIBUTES']
+        for k in attribs:
+            print k,' => ',attribs[k]
+    if 'CAS_USERNAME' in session:
+        is_logged_in = True
+        username = session['CAS_USERNAME']
+        print('CAS_USERNAME is: ',username)
+    else:
+        is_logged_in = False
+        username = None
+        print('CAS_USERNAME is not in the session')
+    return render_template('login.html', username=username, is_logged_in=is_logged_in)
+ 
+ 
+@app.route('/scott/')
+def logged_out():
+    flash('successfully logged out!')
+    return redirect( url_for('index') )
+
+@app.route('/home/', methods= ['GET','POST'])
 def index():
     ''' Index page
     '''
@@ -57,5 +98,9 @@ def pref():
     return render_template('preferences.html')
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        port=int(sys.argv[1])
+    else:
+        port=os.getuid()
     app.debug = True
-    app.run('0.0.0.0',os.getgid())
+    app.run('0.0.0.0',port)
