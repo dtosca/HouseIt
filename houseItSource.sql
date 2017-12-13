468762ceadafa79dfcb765c03266dde171da07a5
team Diana Tosca <houseit@tempest.wellesley.edu>
2017-12-13 11:58:53 -0500

    changed table syntax

--- houseItSource.sql
+++ houseItSource.sql
@@ -6,16 +6,15 @@ create table room (
        room_code varchar(6) primary key,
        dorm enum('Beebe', 'Cazenove', 'Munger', 'Pomeroy', 'Shafer', 
               'Claflin', 'Lake House', 'Severance', 'Tower Court', 
-              'Bates', 'Freeman','McAfee','Casa Cervantes','Dower',
+             'Bates', 'Freeman','McAfee','Casa Cervantes','Dower',
               'French House','Stone-Davis', 'Simpson West', 'Hemlock'),
-       type enum('single', 'double', 'triple', 'quad', 'suite'),
+       room_type enum('single', 'double', 'triple', 'quad', 'suite'),
        hardwood boolean,
        pets_ok boolean,
        accessible boolean,
        available boolean,
        username varchar(15),
        foreign key (username) references student(username)
-
 );
 
 create table student (
@@ -27,14 +26,13 @@ create table student (
               'Claflin', 'Lake House', 'Severance', 'Tower Court', 
               'Bates', 'Freeman','McAfee','Casa Cervantes','Dower',
               'French House','Stone-Davis', 'Simpson West', 'Hemlock'),
-       type enum('single', 'double', 'triple', 'quad', 'suite'),
+       room_type enum('single', 'double', 'triple', 'quad', 'suite'),
        hardwood boolean,
        pets_ok boolean,
        accessible boolean,
        room_code varchar(6),
        foreign key (room_code) references room(room_code)
 );
-
 
 create table admin (
        username varchar(15) primary key,
@@ -46,9 +44,9 @@ insert into admin (username, nm) values
        ('hgranger', 'Hermione Granger'),
        ('rweasley', 'Ron Weasley');
 
-
--- insert into room (room_code, dorm, type, hardwood, pets_ok, accessible, available, username) values
-
-
+insert into room (room_code, dorm, type, hardwood, pets_ok, accessible, available,username) values
+  ('BAT409','Bates','single',true,true,true,true,NULL),
+  ('SHA419','Shafer','single',true,true,true,true,NULL),
+  ('FRE108','Freeman','double',false,false,true,true,NULL);
 
 -- insert into student(username, nm, yr, lottery_num, dorm, type, hardwood, pets_ok, accessible, room_code) values
