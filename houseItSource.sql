3a64b672017e0768fa4e39c287274d0e79d5a5af
jacquelineyoung8 <jyoung4@wellesley.edu>
2017-12-13 22:58:01 -0500

    made new rooms

--- houseItSource.sql
+++ houseItSource.sql
@@ -45,8 +45,25 @@ insert into admin (username, nm) values
        ('rweasley', 'Ron Weasley');
 
 insert into room (room_code, dorm, room_type, nuts, hardwood, pets_ok, acc, available) values
-  ('BAT409','Bates','single',true,true,true,true,true),
-  ('SHA419','Shafer','single',true,true,true,true,true),
-  ('FRE108','Freeman','double',false,true,false,true,true);
+  ('BAT409','Bates','single',true,false,true,false,true),
+  ('BAT309','Bates','double',false,true,false,true,true),
+  ('BAT209','Bates','single',true,false,false,true,true),
+  ('BAT109','Bates','double',false,false,true,true,true),
+  ('BAT009','Bates','single',true,true,false,false,true),
+  ('SHA419','Shafer','single',false,false,true,true,true),
+  ('SHA319','Shafer','single',true,false,false,true,true),
+  ('SHA219','Shafer','single',false,true,true,true,true),
+  ('SHA119','Shafer','single',true,false,true,true,true),
+  ('SHA019','Shafer','single',true,true,false,true,true),
+  ('FRE408','Freeman','double',false,false,false,true,true);
+  ('FRE308','Freeman','single',false,true,false,true,true);
+  ('FRE208','Freeman','double',true,true,false,true,true);
+  ('FRE108','Freeman','single',true,true,false,true,true);
+  ('FRE008','Freeman','double',false,true,true,true,true);
+  ('TOW423','Tower','double',false,true,true,false,true);
+  ('TOW323','Tower','double',false,true,false,true,true);
+  ('TOW223','Tower','double',false,false,true,true,true);
+  ('TOW123','Tower','double',false,false,false,true,true);
+  ('TOW023','Tower','double',false,true,falsefalse,true,true);
 
 -- insert into student(username, nm, yr, lottery_num, dorm, room_rtype, hardwood, pets_ok, accessible, room_code) values
